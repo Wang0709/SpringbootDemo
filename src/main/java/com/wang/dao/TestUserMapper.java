a7b7ab6ed020a822111a9d3263a81ad81d853431
Wang0709 <806300992@qq.com>
2018-11-14 10:31:26 +0800

    change mapper dir

--- src/main/java/com/wang/dao/TestUserMapper.java
+++ src/main/java/com/wang/dao/TestUserMapper.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.wang.entity.TestUser;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface TestUserMapper {
     int insert(TestUser record);
 
